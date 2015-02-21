a4aae2da499cffd713926419a5a59e405b4a5115
Rodolphe Quiédeville <rodolphe@quiedeville.org>
2015-02-21 14:03:55 -0500

    Replace code char by value

--- gatling-assert.groovy
+++ gatling-assert.groovy
@@ -1,24 +1,24 @@
-def matcher = manager.getLogMatcher(&quot;.*(Global: .* response time is less than .*): false\$&quot;)
+def matcher = manager.getLogMatcher(".*(Global: .* response time is less than .*): false\$")
 if(matcher?.matches()) {
     manager.addWarningBadge(matcher.group(1) )
-    manager.createSummary(&quot;warning.gif&quot;).appendText(matcher.group(1), false, false, false, &quot;red&quot;)
+    manager.createSummary("warning.gif").appendText(matcher.group(1), false, false, false, "red")
 }
 
-def pmatcher = manager.getLogMatcher(&quot;.*(Global percentage of requests OK is greater than .*): false\$&quot;)
+def pmatcher = manager.getLogMatcher(".*(Global percentage of requests OK is greater than .*): false\$")
 if(pmatcher?.matches()) {
     manager.addWarningBadge(pmatcher.group(1) )
-    manager.createSummary(&quot;error.gif&quot;).appendText(pmatcher.group(1), false, false, false, &quot;red&quot;)
+    manager.createSummary("error.gif").appendText(pmatcher.group(1), false, false, false, "red")
 }
 
-def fqr = manager.getLogMatcher(&quot;.*(Global number of requests KO is equal to .*: false)\$&quot;)
+def fqr = manager.getLogMatcher(".*(Global number of requests KO is equal to .*: false)\$")
 if(fqr?.matches()) {
     manager.addWarningBadge(fqr.group(1) )
-    manager.createSummary(&quot;error.gif&quot;).appendText(fqr.group(1), false, false, false, &quot;red&quot;)
+    manager.createSummary("error.gif").appendText(fqr.group(1), false, false, false, "red")
 }
 
-def report = manager.getLogMatcher(&quot;&gt;.*(max reponse time .*  generated in .*)s.\$&quot;)
+def report = manager.getLogMatcher("&gt;.*(max reponse time .*  generated in .*)s.\$")
 if(report?.matches()) {
-    manager.createSummary(&quot;info.gif&quot;).appendText(report.group(1), false, false, false, &quot;black&quot;) 
+    manager.createSummary("info.gif").appendText(report.group(1), false, false, false, "black") 
 }
 
 def version = manager.getLogMatcher(".*version=(.*) Redirect.*\$")
